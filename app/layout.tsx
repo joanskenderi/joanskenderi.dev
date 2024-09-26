1f62c9c36e23688f1524cb4f80f7f6e57c65cc10
joanskenderi <jo.skenderi@gmail.com>
2024-09-26 22:39:18 +0200

    feat: add favicon, font and SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,31 @@
+import type { Metadata } from 'next';
+import { Titillium_Web } from 'next/font/google';
+
 import './globals.css';
+
+const TitilliumWeb = Titillium_Web({
+  subsets: ['latin'],
+  weight: ['700'],
+  preload: true,
+});
+
+export const metadata: Metadata = {
+  title: 'Joan Skënderi | Portfolio',
+  description:
+    'I am Joan Skënderi, a software developer focused in JavaScript. Discover skills, experiences, contact information and more.',
+  keywords: [
+    'Joan Skënderi',
+    'Joan Skenderi',
+    'joan skenderi',
+    'joanskenderi',
+    'Software Developer',
+    'Full Stack Developer',
+    'JavaScript Developer',
+  ],
+  authors: {
+    name: 'Joan Skënderi',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +34,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${TitilliumWeb.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
